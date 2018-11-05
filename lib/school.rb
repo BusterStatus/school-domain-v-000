6ef33c6a6a2eed3e24e4f280c5e2ab54e48032bf
matthew.serre <matthew.serre@gmail.com>
2018-11-05 03:36:15 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
   end
   
   def add_student(student_name,grade)
-    @roster[grade] = []
+    @roster[grade] = [] if @roster.include?(grade) == false
     @roster[grade] << "#{student_name}"
   end
   
